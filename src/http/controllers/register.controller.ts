13a70f17938631232277da080ba7309d87221e48
Bruno Santos <brsantos197@gmail.com>
2024-07-16 00:29:34 -0300

    new: Caso de Uso e Design Patterns - Caso de uso de registro

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,7 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { registerUseCase } from '../use-cases/register'
 
 export const register = async (
   request: FastifyRequest,
@@ -14,26 +13,15 @@ export const register = async (
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
-
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  try {
+    await registerUseCase({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
+      password,
+    })
+  } catch (err) {
     return reply.status(409).send()
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
-
-  reply.status(201).send()
+  return reply.status(201).send()
 }

18bdbc7ccc3cc7d681a84ff089d57be0133a87d2
Bruno Santos <brsantos197@gmail.com>
2024-12-08 05:10:18 -0300

    refactor(controllers): implementa factory pattern para casos de uso
    - Substitui instanciação direta dos casos de uso por funções factory
    - Remove importações não utilizadas
    - Melhora a modularidade e testabilidade do código

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
-import { RegisterUseCase } from '@/use-cases/register'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -16,8 +15,7 @@ export const register = async (
 
   const { name, email, password } = registerBodySchema.parse(request.body)
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const registerUseCase = makeRegisterUseCase()
 
     await registerUseCase.execute({
       name,

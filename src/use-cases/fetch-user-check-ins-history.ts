3ba9cb61f3f79f19c264dc0c50d86bd62f0287f0
Bruno Santos <brsantos197@gmail.com>
2024-12-09 13:42:11 -0300

    feat: adiciona histórico de check-ins do usuário
    - Implementa funcionalidade para obter histórico de check-ins do usuário
    - Adiciona paginação para listas de dados (20 itens por página)
    - Atualiza README com novas funcionalidades concluídas
    - Corrige descrições de testes para melhor clareza

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface CheckInseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<CheckInseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
